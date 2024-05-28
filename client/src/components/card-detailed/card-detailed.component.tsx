accc68cbcd0a3be12d7f618a23accb1d6aa8c00e
ztarknap <ztarknap>
2024-05-28 19:29:32 +0700

    logfile and tooltips

--- client/src/components/card-detailed/card-detailed.component.tsx
+++ client/src/components/card-detailed/card-detailed.component.tsx
@@ -1,13 +1,17 @@
 import { CardDetailedType } from "../../utils/ts_types"
+import { Tooltip } from "@mui/material";
 import './card-detailed.styles.scss'
 import { useState } from "react";
 
 export const CardDetailed = (card:CardDetailedType) => {
     console.log(card);
-    const [isHover, isHoverSet] = useState(false);
+
+    let card_description = 'Just a plain card!';
     return (
         <span className="card-container">
+           <Tooltip title={`${card_description}`} placement="right">
            <img src ={`/svg-cards/${card.img}`}></img>
+            </Tooltip>
         </span>
     )
 }
--- client/src/routes/play/play.component.tsx
+++ client/src/routes/play/play.component.tsx
@@ -1,12 +1,26 @@
+import { Button } from '@mui/material';
 import {wsSend} from '../../ws_utils/ws-play-connection';
 import { playStartEvent } from '../../ws_utils/ws_api/build-event';
+import { useState } from 'react';
+import './play.styles.scss'
+
+const appendLogs = (prevLogs:string, newLog:string) => {
+    return prevLogs.concat('\n', newLog);
+}
+
 
 export const Play = () => {
     wsSend(playStartEvent("test"));
+    const [logs, logsSet] = useState('');
+
     
+
     return(
         <div>
-            play
+            
+           <div> <textarea id="logs_box" value={logs}/></div>
+            <Button variant="contained" color='secondary' onClick={() => {logsSet(appendLogs(logs,"Aaa"))}}> Play</Button>
+                 
         </div>
     )
 }
